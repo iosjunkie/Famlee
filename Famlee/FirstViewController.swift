07dc47d7c6f9822aef32e0bccd466d241bc0c40c
Jules Lee <jackconsolidated@gmail.com>
2019-07-21 12:32:23 +0800

    Initial Commit

---
+++ Famlee/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Famlee
+//
+//  Created by Jules Lee on 21/07/2019.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
